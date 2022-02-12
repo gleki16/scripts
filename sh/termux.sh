70999423196a378ef77367b5a5923c07ffcb9d28
ray <rraayy246@gmail.com>
2022-02-12 09:12:39 +0800

    uz to script repo

--- sh/termux.sh
+++ sh/termux.sh
@@ -3,6 +3,6 @@
 pkg upgrade -y
 pkg install -y curl fish
 
-curl -fLo $HOME/termux.fish https://gitlab.com/glek/uz/raw/main/scr/termux.fish
+curl -fLo $HOME/termux.fish https://gitlab.com/glek/scripts/raw/main/sh/termux.fish
 fish $HOME/termux.fish
 rm $HOME/termux.fish
