adca9ddd79409b054d96c20f475124ab60da7589
ray <rraayy246@gmail.com>
2022-02-17 10:58:37 +0800

    desc

--- sh/transactional-update.sh
+++ sh/transactional-update.sh
@@ -225,7 +225,7 @@ create_snapshot() {
 		desc+=("sh")
 	fi
 
-	local snapshot_id=`snapper create --print-number --cleanup-algorithm=number --description=${desc[*]}`
+	local snapshot_id=`snapper create --print-number --cleanup-algorithm=number --description="${desc[*]}"`
 	snapshot_dir="/.snapshots/$snapshot_id/snapshot"
 
 	new_snapshot_action
