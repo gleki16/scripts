0f82aee512a900246b74305a0fa0d2c0f8896be1
ray <rraayy246@gmail.com>
2022-02-28 11:36:55 +0800

    btrfs-scrub

--- sh/arch.sh
+++ sh/arch.sh
@@ -697,7 +697,8 @@ set_wallpaper() {
 set_auto_start() {
 	local mask_list=(systemd-resolved)
 	local disable_list=(systemd-timesyncd)
-	local enable_list=(chronyd dnscrypt-proxy fcron grub-btrfs.path nftables paccache.timer pkgstats.timer sshd)
+	local btrfs_scrub="btrfs-scrub@$(systemd-escape -p /).timer"
+	local enable_list=(${btrfs_scrub} chronyd dnscrypt-proxy fcron grub-btrfs.path nftables paccache.timer pkgstats.timer sshd)
 
 	if [ "$use_gui" = 1 ]; then
 		# dhcpcd 和 NetworkManager 不能同时启动
