d9f37ce59883dd20f9d879c7953859364ea50e2b
ray <rraayy246@gmail.com>
2022-03-24 16:31:13 +0800

    a.sh

--- sh/a.sh
+++ sh/a.sh
@@ -1,1 +1,5 @@
-rsync -ah --info=progress2 --delete --inplace --no-whole-file "$@"
+rsync -ahnv --delete "$@"
+read -p 'are you sure? ' sure
+if [ "$sure" = '' -o "$sure" = 'y' ]; then
+  rsync -ah --info=progress2 --delete --inplace --no-whole-file "$@"
+fi
