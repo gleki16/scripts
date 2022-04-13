9b58cb084e3d8de464a1d7d6ca8061c3eb99fc23
ray <rraayy246@gmail.com>
2022-04-13 14:21:34 +0800

    .zshrc

--- sh/arch.sh
+++ sh/arch.sh
@@ -622,6 +622,9 @@ set_ipfs() {
 
 set_shell() {
 	sed -i '/home\|root/s/bash/zsh/' /etc/passwd
+
+	echo "# 如果没有 .zshrc，zsh 会要你新建一个" > ${user_home}/.zshrc
+	echo "# 如果没有 .zshrc，zsh 会要你新建一个" > /root/.zshrc
 }
 
 set_snapper() {
