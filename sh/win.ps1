79bf1721d0e10119eb0dcecd13a2316f96749818
游元 <rraayy246@gmail.com>
2022-05-23 10:16:08 +0000

    全用 winget，scoop 太折磨人了

--- sh/win.ps1
+++ sh/win.ps1
@@ -1,14 +1,7 @@
-#  安装 Scoop
-if(-not(test-path ~/scoop)) {
-    Invoke-WebRequest get.scoop.sh | Invoke-Expression
-}
-
-# 下载默认软件
-winget install 7zip.7zip
-winget install VideoLAN.VLC
-
-# 添加软件源
-scoop bucket add extras
-
 # 下载常用软件
-scoop install git powertoys firefox bulk-crap-uninstaller
+winget install 7zip.7zip
+winget install Git.Git
+winget install Klocman.BulkCrapUninstaller
+winget install Microsoft.PowerToys
+winget install Mozilla.Firefox
+winget install VideoLAN.VLC
