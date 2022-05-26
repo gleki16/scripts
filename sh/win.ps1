5faf36a956d56147465b9cdb590e5659f335bdb1
ray <rraayy246@gmail.com>
2022-05-26 13:10:31 +0800

    a

--- sh/win.ps1
+++ sh/win.ps1
@@ -1,7 +1,5 @@
 # 下载常用软件
 winget install 7zip.7zip
-winget install Git.Git
 winget install Klocman.BulkCrapUninstaller
 winget install Microsoft.PowerToys
 winget install Mozilla.Firefox
-winget install VideoLAN.VLC
