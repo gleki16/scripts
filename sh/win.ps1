a52d145ecdcb14d73e20d92bb2c39ceb0c397d06
游元 <rraayy246@gmail.com>
2022-06-22 05:55:40 +0000

    小鹤双拼 键位

--- sh/win.ps1
+++ sh/win.ps1
@@ -3,3 +3,8 @@ winget install 7zip.7zip
 winget install Klocman.BulkCrapUninstaller
 winget install Microsoft.PowerToys
 winget install Mozilla.Firefox
+
+# 小鹤双拼 键位
+Invoke-WebRequest https://raw.githubusercontent.com/2015WUJI01/xhup-for-win10/master/xhup.reg -OutFile "xhup.reg"
+regedit /s "xhup.reg"
+Remove-Item "xhup.reg"
