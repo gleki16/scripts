e1d76351d46570a67eae920b9fa1200bc0a8fdb5
游元 <rraayy246@gmail.com>
2022-06-22 07:38:03 +0000

    UTC 时间

--- sh/win.ps1
+++ sh/win.ps1
@@ -8,3 +8,6 @@ winget install Mozilla.Firefox
 Invoke-WebRequest https://raw.githubusercontent.com/2015WUJI01/xhup-for-win10/master/xhup.reg -OutFile "xhup.reg"
 regedit /s "xhup.reg"
 Remove-Item "xhup.reg"
+
+# UTC 时间 (需要管理员权限)
+Reg add HKLM\SYSTEM\CurrentControlSet\Control\TimeZoneInformation /v RealTimeIsUniversal /t REG_DWORD /d 1
